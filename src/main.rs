23f40c8898cb468441944e0d45e62ad42cb916f2
Julian Phillips <julian@quantumfyre.co.uk>
2025-04-11 20:42:03 +0100

    Start building a CLI
    Start putting together a CLI interface that we can use to start building
    functionality.
    
    We can't do anything until we have an interface, so let's start putting
    that together first.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Parser, Subcommand};
+
+pub const CLAP_STYLING: clap::builder::styling::Styles = clap::builder::styling::Styles::styled()
+    .header(clap_cargo::style::HEADER)
+    .usage(clap_cargo::style::USAGE)
+    .literal(clap_cargo::style::LITERAL)
+    .placeholder(clap_cargo::style::PLACEHOLDER)
+    .error(clap_cargo::style::ERROR)
+    .valid(clap_cargo::style::VALID)
+    .invalid(clap_cargo::style::INVALID);
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(styles = CLAP_STYLING)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// store a shell command in the history
+    Store{
+        /// the shell command to be stored
+        shell_command: String,
+    },
+
+    /// list all the stored commands
+    History,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Store{shell_command: command} => println!("store: {command}"),
+        Commands::History => println!("history"),
+    }
 }
